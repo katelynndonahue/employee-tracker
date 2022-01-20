388f318a29380a49f06543aed5d52444ae3c5be7
k.donahue45@yahoo.com <k.donahue45@yahoo.com>
2022-01-20 17:22:13 -0500

    updated seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,10 +7,17 @@
 
 INSERT INTO department (name) VALUES 
 ("Sales"),
-("Audits");
+("Audits"),
+("Accounting"),
+("Operations"),
+("");
 
 INSERT INTO role (title, salary, department_id) VALUES
-("Sales Manager", 120000, 1);
+("Sales Manager", 120000, 1),
+("Audit Manager", 100,000, 2),
+("Accounting Manager", 110,000, 3),
+("Operations Manager", 105,000, 4),
+;
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
 ("FIRST NAME", "LAST NAME", ROLE, MANAGER ID);
