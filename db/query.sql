cee5bf35bd99d1226f04659bd18b65d1011180d6
k.donahue45@yahoo.com <k.donahue45@yahoo.com>
2022-01-19 22:01:16 -0500

    edits to query.sql

--- db/query.sql
+++ db/query.sql
@@ -1,1 +1,7 @@
-SUM(expression)
+-- Add your code below and execute file in MySQL Shell --
+SELECT employee_db, employee_name
+FROM TABLE id
+LEFT JOIN pay_rate
+ON job_title, pay_rate = pay_rate
+ORDER BY pay_rate;
+
